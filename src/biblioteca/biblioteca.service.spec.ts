fe2b0473cd77177b9a4ce139bba899bfbec9209c
Juan Pablo Rodriguez <jp.rodriguez22@uniandes.edu.co>
2025-05-16 22:36:01 -0500

    Pruebas de biblioteca hasta creación

--- src/biblioteca/biblioteca.service.spec.ts
+++ src/biblioteca/biblioteca.service.spec.ts
@@ -42,4 +42,71 @@ describe('BibliotecaService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('List all libraries', async () => {
+    const result: BibliotecaEntity[] = await service.findAll();
+    expect(result).not.toBeNull()
+    expect(result.length).toBe(bibliotecaList.length);
+  })
+
+  it('Find a library', async () => {
+    const search: BibliotecaEntity = await bibliotecaList[0];
+    const result: BibliotecaEntity = await service.findOne(search.id);
+    expect(result).not.toBeNull()
+    expect(result.id).toBe(search.id)
+  })
+
+  it('Library does not exist', async () => {
+    const fakeID = 3423656
+    await expect(() => service.findOne(fakeID)).rejects.toHaveProperty('message', `La biblioteca con id ${fakeID} no existe en el sistema`)
+  })
+
+  it('Create a new library', async () => {
+    const newLibrary: BibliotecaEntity = {
+      id: 6,
+      nombre: faker.company.name(),
+      direccion: faker.location.direction(),
+      ciudad: faker.location.city(),
+      horaApertura: '09:00',
+      horaCierre: '18:00',
+      libros: []
+    }
+    const result: BibliotecaEntity = await service.create(newLibrary);
+    expect(result).not.toBeNull()
+    const storedResult: BibliotecaEntity | null = await repository.findOne({ where: { id: result.id } });
+    expect(storedResult).not.toBeNull()
+    expect(storedResult!.nombre).toBe(newLibrary.nombre)
+  })
+
+  it('Create a new library wrong time format', async () => {
+    const newLibrary: BibliotecaEntity = {
+      id: 7,
+      nombre: faker.company.name(),
+      direccion: faker.location.direction(),
+      ciudad: faker.location.city(),
+      horaApertura: '09',
+      horaCierre: '11',
+      libros: []
+    }
+    await expect(() => service.create(newLibrary)).rejects.toHaveProperty('message', `El tiempo de apertura debe estar entre 0:00 y 23:59 horas`)
+  })
+
+  it('Create a new library closing time before opening time', async () => {
+    const newLibrary: BibliotecaEntity = {
+      id: 7,
+      nombre: faker.company.name(),
+      direccion: faker.location.direction(),
+      ciudad: faker.location.city(),
+      horaApertura: '09:00',
+      horaCierre: '04:00',
+      libros: []
+    }
+    await expect(() => service.create(newLibrary)).rejects.toHaveProperty('message', `El tiempo de apertura debe ser menor al de cierre`)
+  })
+
+  it('Update a library', async () => {
+    
+  })
+
 });
+
